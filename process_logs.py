68ab5ff45384e27e1c94c98174630a3599ac95f2
Andrew Jennings <andrewjs@sas.upenn.edu>
2015-07-22 11:08:58 -0400

    Step 3a: Read that regex
    Accept the regex input. Print out all matches of the pattern.

--- process_logs.py
+++ process_logs.py
@@ -1,17 +1,27 @@
 import argparse
+import re
 
 parser = argparse.ArgumentParser(description='Read logs and total some number')
 
-# Grab the requested file
+# Process our arguments
+parser.add_argument("regex")
 parser.add_argument("filename")
 args = parser.parse_args()
+
+# Grab the requested file
 log_file = open(args.filename, 'r')
 
 # Read each line of the file
+p = re.compile(args.regex)
 for line in log_file:
     print line
+    # See whether it matches the regex
+    matches = p.findall(line)
+    for index, match in enumerate(matches):
+        print "Match number %d is %s" % (index, match)
 
-# See whether it matches the regex
 # Get the number from the line if it does
 # Add the number to our total
 # Return our total
+
+log_file.close()
