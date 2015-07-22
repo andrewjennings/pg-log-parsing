db7b27f2e57bad9a4edc6c1e825bb110009b00ce
Andrew Jennings <andrewjs@sas.upenn.edu>
2015-07-22 10:43:33 -0400

    Step 2: Read each line of the file

--- process_logs.py
+++ process_logs.py
@@ -5,12 +5,12 @@ parser = argparse.ArgumentParser(description='Read logs and total some number')
 # Grab the requested file
 parser.add_argument("filename")
 args = parser.parse_args()
-print "Filename is %s " % args.filename
 log_file = open(args.filename, 'r')
-print "File is %s" % log_file
-
 
 # Read each line of the file
+for line in log_file:
+    print line
+
 # See whether it matches the regex
 # Get the number from the line if it does
 # Add the number to our total
