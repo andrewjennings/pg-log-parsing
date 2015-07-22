745789865aaccb3ba8a4abbac4fd17abf8cfdf1e
Andrew Jennings <andrewjs@sas.upenn.edu>
2015-07-22 10:36:29 -0400

    Step 1: Grab the file.

--- process_logs.py
+++ process_logs.py
@@ -1,9 +1,15 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Read logs and total some number')
-parser.parse_args()
 
 # Grab the requested file
+parser.add_argument("filename")
+args = parser.parse_args()
+print "Filename is %s " % args.filename
+log_file = open(args.filename, 'r')
+print "File is %s" % log_file
+
+
 # Read each line of the file
 # See whether it matches the regex
 # Get the number from the line if it does
